91f2b2d1c3454198ecde752876868ab07c8963cd
Your Name <you@example.com>
2021-01-27 21:36:27 -0600

    7 - login page updates

--- friends/src/components/LoginPage.js
+++ friends/src/components/LoginPage.js
@@ -1,13 +1,36 @@
 import React from 'react'
 
 function LoginPage() {
+    const [formValues, setFormValues] = useState({
+        username:'', 
+        password: '',
+    });
+
+    const handleChanges = e => {
+        const { name, value } = e.target;
+        setFormValues({
+            ... formValues,
+            [name]: value
+        })
+    }
+
+    const login = e => {
+        e.preventDefault();
+        
+    }
+
     return (
         <div>
             <form onSubmit={login}>
                 <label htmlFor="username">username</label>
-                <input id="username" value={formValues.username} onChange={handleChanges} />
+                <input  id="username"
+                        value={formValues.username}
+                        onChange={handleChanges} />
                 <label htmlFor="password">password</label>
-                <input id="password" value={formValues.password} onChange={handleChanges} />
+                <input 
+                        id="password" 
+                        value={formValues.password} 
+                        onChange={handleChanges} />
             </form>
         </div>
     );
