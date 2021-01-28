86cc53e7336995123d01c6c4c2cdba69918c7e23
Your Name <you@example.com>
2021-01-27 21:27:05 -0600

    6 - updating

--- friends/src/components/LoginPage.js
+++ friends/src/components/LoginPage.js
@@ -3,9 +3,14 @@ import React from 'react'
 function LoginPage() {
     return (
         <div>
-            
+            <form onSubmit={login}>
+                <label htmlFor="username">username</label>
+                <input id="username" value={formValues.username} onChange={handleChanges} />
+                <label htmlFor="password">password</label>
+                <input id="password" value={formValues.password} onChange={handleChanges} />
+            </form>
         </div>
-    )
+    );
 }
 
 export default LoginPage
